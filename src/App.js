6a1750d7eaf316fb0795d3171f880ba4e091b28e
Archer <zaib_qasim@hotmail.com>
2019-05-21 22:34:54 +0500

    Firebase config + input field setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyAgRj7jODquw9KwpaQ14PUPZB4mwzOm13c',
+			authDomain: 'authentication-c64af.firebaseapp.com',
+			databaseURL: 'https://authentication-c64af.firebaseio.com',
+			projectId: 'authentication-c64af',
+			storageBucket: 'authentication-c64af.appspot.com',
+			messagingSenderId: '579471061921',
+			appId: '1:579471061921:web:6c98e1958befda7b'
+		});
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>Hello World</Text>
+				<LoginForm />
 			</View>
 		);
 	}
